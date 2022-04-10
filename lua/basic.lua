cc7859e956e6ae7a9d485138ddd1697b18ae5994
akashic.zhang <2095138755@qq.com>
2022-04-10 23:16:29 +0800

    basic options

---
+++ lua/basic.lua
@@ -0,0 +1,87 @@
+-- UTF-8
+vim.g.encoding = "UTF-8"
+vim.o.fileencoding = 'UTF-8'
+-- 移动光标时周围保留8行
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+-- 使用相对行号
+vim.wo.number = true
+vim.wo.relativenumber = true
+-- 高亮所在行
+vim.wo.cursorline = true
+-- 显示左侧图标指示列
+vim.wo.signcolumn = "yes"
+-- 右侧参考线 超过考虑换行
+vim.wo.colorcolumn = "80"
+-- 缩进2个空格等于一个Tab
+vim.o.tabstop = 2
+vim.bo.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftround = true
+-- 缩进时移动长度
+vim.o.shiftwidth = 2
+vim.bo.shiftwidth = 2
+-- 空格替代tab
+vim.o.expandtab = true
+vim.bo.expandtab = true
+-- 新行对齐当前行
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.smartindent = true
+-- 搜索大小写不敏感 除非包含大写
+vim.o.ignorecase = true
+vim.o.smartcase = true
+-- 搜索高亮
+vim.o.hlsearch = true
+-- 边输入边搜索
+vim.o.incsearch = true
+-- 命令行高为2 提供足够的显示空间
+vim.o.cmdheight = 2
+-- 当文件被外部程序修改时 自动加载
+vim.o.autoread = true
+vim.bo.autoread = true
+-- 禁止折行
+vim.wo.wrap = false
+-- 光标在行首尾时<Left><Right>可以跳到下一行
+vim.o.whichwrap = '<,>,[,]'
+-- 允许隐藏被修改过的buffer
+vim.o.hidden = true
+-- 鼠标支持
+vim.o.mouse = "a"
+-- 禁止创建备份文件
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+-- 更快补全 默认4000ms
+vim.o.updatetime = 300
+-- 等待键盘快捷键连击时间
+vim.o.timeoutlen = 300
+-- split window 从下边和右边出现
+vim.o.splitbelow = true
+vim.o.splitright = true
+-- 自动补全不自动选中
+vim.g.completeopt = "menu,menuone,noselect,noinsert"
+-- 样式
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.opt.termguicolors = true
+-- 补全增强
+vim.o.wildmenu = true
+-- Dont' pass messages to |ins-completin menu|
+vim.o.shortmess = vim.o.shortmess .. 'c'
+-- 补全最多显示10行
+vim.o.pumheight = 10
+-- 永远显示 tabline
+vim.o.showtabline = 2
+-- 使用增强状态栏插件后不再需要vim的模式提示
+vim.o.showmode = false
+-- 配置剪切板
+vim.opt.clipboard = "unnamedplus"
+
+-- 撤销记录
+-- vim.opt.undofile = true
+-- 行号列宽
+-- vim.opt.numberwidth = 4
+-- 不可见字符的显示
+-- vim.o.list = true
+-- vim.o.listchars = "space:·"
